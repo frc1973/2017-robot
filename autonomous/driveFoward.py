510ae6440d83eca4e15c28f5fb82b9b3cd565123
vilarj <cjimenez18@bostonk12.org>
2017-03-03 15:44:57 -0500

    Create Autonomynus state, made it drive, etc.

---
+++ autonomous/driveFoward.py
@@ -0,0 +1,20 @@
+
+
+from magicbot import AutonomousStateMachine, timed_state, state
+import wpilib
+
+class DriveFoward(AutonomousStateMachine):
+
+    MODE_NAME = 'Drive Foward'
+    DEFAULT = True
+
+    myRobot = wpilib.RobotDrive
+
+    @timed_state(duration=3,next_state ="stop", first=True)
+    def drive_Foward(self):
+        self.myRobot.drive(-0.5,0)
+
+    @state
+    def stop(self):
+        self.myRobot.drive(0,0)
+        self.done()
--- robot.py
+++ robot.py
@@ -4,18 +4,36 @@ import wpilib
 from magicbot import MagicRobot
 
 class MyRobot(MagicRobot):
-    
+
     #
     # Define components here
     #
-    
+
     def createObjects(self):
         """Initialize all wpilib motors & sensors"""
-        
+
+        # camera
+        # utrasoni sensors
+        # motors
+        self.myRobot = wpilib.RobotDrive(0,1)
+        self.myRobot.setSafetyEnabled(False)
+
+        #2Joysticks
+        self.leftStick = wpilib.Joystick(0)
+        self.rightStick = wpilib.Joystick(1)
+
+        # 5motor controlors: 1colocter, 2 for weels, 1 for shooter
+        #light
+        #lifter: 1 motor
+        #
+
+
+
     def teleopPeriodic(self):
         """Place code here that does things as a result of operator
            actions. This code gets called over and over again, do not
            put a loop in"""
-           
+
+        self.myRobot.arcadeDrive(self.leftStick, True)
 if __name__ == '__main__':
     wpilib.run(MyRobot)
